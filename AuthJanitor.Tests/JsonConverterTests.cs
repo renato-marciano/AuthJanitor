16de69d154f8efb5394ce56e64b4b26e43cecc75
renato <remarcia@microsoft.com>
2020-06-11 18:08:08 -0400

    Added Tests for int & long Json Serialization

---
+++ AuthJanitor.Tests/JsonConverterTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text.Json;
+using AuthJanitor.Integrations.IdentityServices.AzureActiveDirectory;
+using Xunit;
+
+namespace AuthJanitor.Tests
+{
+    public class JsonConverterTests
+    {
+
+        [Fact]
+        public void JsonSerializerShouldReturnStringForNumbers()
+        {
+            string expected = "{\"MemberInt\":\"5\",\"MemberLong\":\"1\"}";
+
+            var writeAsString = true;
+            string tokenJson = JsonSerializer.Serialize(SampleDTO(), GetSerializerOptions(writeAsString));
+
+            Assert.Equal(expected, tokenJson);
+        }
+
+        [Fact]
+        public void JsonSerializerShouldReturnNumbersForNumbers()
+        {
+            string expected = "{\"MemberInt\":5,\"MemberLong\":1}";
+
+            var writeAsString = false;
+            string tokenJson = JsonSerializer.Serialize(SampleDTO(), GetSerializerOptions(writeAsString));
+
+            Assert.Equal(expected, tokenJson);
+        }
+
+        private JsonSerializerOptions GetSerializerOptions(bool writeAsString)
+        {
+            return new JsonSerializerOptions()
+            {
+                Converters = { new StringToIntJsonConverter(writeAsString), new StringToLongJsonConverter(writeAsString) }
+            };
+        }
+
+        private TestDTO SampleDTO()
+        {
+            return new TestDTO()
+            {
+                MemberInt = 5,
+                MemberLong = 1L
+            };
+        }
+
+    }
+
+    public class TestDTO
+    {
+        public int MemberInt { get; set; }
+        public long MemberLong { get; set; }
+    }
+
+}
